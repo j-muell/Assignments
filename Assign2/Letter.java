ef4f7901d4e0e9a7b700f51b572e3438457de634
Jason Mueller <jasonmueller17@yahoo.ca>
2022-02-21 17:10:57 -0500

    letter finished

--- Assign2/Letter.java
+++ Assign2/Letter.java
@@ -22,6 +22,51 @@ public class Letter {
     }
 
     public String decorator(){
+        if (this.label == UNUSED) {
+            return "-";
+        } else if (this.label == USED) {
+            return "+";
+        } else if (this.label == UNSET) {
+            return " ";
+        }
+
+        return "!"; // if the label is none of the above, it has to be CORRECT, therefore return !
+    }
+
+    public String toString() {
+        return decorator() + letter + decorator();
+    }
+
+    public void setUnused() {
+        label = UNUSED;
+    }
+
+    public void setUsed() {
+        label = USED;
+    }
+
+    public void setCorrect() { 
+        label = CORRECT;
+    }
+
+    public boolean isUnused() {
+        boolean distinction = this.label == UNUSED ? true : false;
+        return distinction;
+    }
+
+    public static Letter[] fromString(String s) {
+        Letter[] letters;
+        String givenWord = s;
+
+        letters = new Letter[givenWord.length()]; // initialize the letters array with the legnth of the given word
+
+        for (int i = 0; i < givenWord.length() - 1; i++ ){   
+            letters[i] = new Letter(givenWord.charAt(i)); // each index of the letter array is given a new letter with 
+                                                         // the character at that respective index
+        }
+
+        return letters; // in the end return letters array
 
     }
+
 }
