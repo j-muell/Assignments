adaeba9f3aaf989b72bd4077e8bf48482550d585
Jason Mueller <jasonmueller17@yahoo.ca>
2022-03-10 12:54:46 -0500

    DLStack

---
+++ Assign3/DLStack.java
@@ -0,0 +1,49 @@
+public class DLStack<T> implements DLStackADT<T>{
+    private DoubleLinkedNode<T> top;
+    private int numItems;
+
+    public DLStack() {
+        /**
+         * Create an empty stack using doublylinkedlist
+         */
+    }
+
+    public void push(T dataItem) {
+
+    }
+
+    public T pop() throws EmptyStackException {
+        /**
+         * Remove and return top element of the stack
+         */
+    }
+
+    public T pop(int k) throws InvalidItemException {
+        /**
+         * Removes and returns the kth item from the stack.
+         */
+    }
+
+    public T peek() throws EmptyStackException {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() { 
+
+    }
+
+    public DoubleLinkedNode<T> getTop() {
+
+    }
+
+    public String toString() {
+        
+    }
+
+
+
+}
