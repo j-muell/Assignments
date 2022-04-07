612e96b6202a52f153a8814d40de0d4860728717
j-muell <jasonmueller17@yahoo.ca>
2022-04-06 22:04:00 -0400

    finished

--- Assign4/Ski.java
+++ Assign4/Ski.java
@@ -6,29 +6,26 @@ public class Ski {
         // takes in an array of possible ski types
         SkiSegment[] segments = new SkiSegment[data.length];
 
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) { // traverse the data
 
-            if (data[i] == null) {
+            if (data[i] == null) { // check for null
                 segments[i] = null;
-                continue;
+                continue; // continue traversing the data
             }
 
-            if (data[i].contains("jump")) {
+            if (data[i].contains("jump")) { // check if the data contains jump
                 segments[i] = new JumpSegment(String.valueOf(i), data[i]);
-                continue;
+                continue; // continue traversing the data
             }
 
-            else if (data[i].contains("slalom")) {
+            else if (data[i].contains("slalom")) { // check if data contains slalom
                 segments[i] = new SlalomSegment(String.valueOf(i), data[i]);
-                continue;
+                continue; // continue traversing the data
             }
 
-            segments[i] = new SkiSegment(String.valueOf(i), data[i]);
-
+            segments[i] = new SkiSegment(String.valueOf(i), data[i]); // if none of the other is true, the data piece has to be a normal segment
+            
         }
-
-
-
 
         TreeBuilder<SkiSegment> newTreeObj = new TreeBuilder<SkiSegment>();
 
@@ -43,9 +40,10 @@ public class Ski {
 
     public void skiNextSegment(BinaryTreeNode<SkiSegment> node, ArrayUnorderedList<SkiSegment> sequence) {
         
+
         sequence.addToRear(node.getData());
         if (node.getLeft() == null && node.getRight() == null) return; // base case is when the node is a leaf (left and right have nothing in them)
-
+        
         skiNextSegment(testRightLeft(node), sequence);
 
     }
@@ -86,8 +84,10 @@ public class Ski {
             SlalomSegment slalomSegLeft = (SlalomSegment) leftNode.getData();
             SlalomSegment slalomSegRight = (SlalomSegment) rightNode.getData();
 
-            if (slalomSegRight.getDirection() == "W") return leftNode; // if the right node contains W, return left 
-            if (slalomSegLeft.getDirection() == "W") return rightNode; // if the left node contains W, return right
+            if (slalomSegRight.getDirection().equals("L")) 
+            return rightNode; // if the right node contains W, return left 
+            if (slalomSegLeft.getDirection().equals("L")) 
+            return leftNode; // if the left node contains W, return right
         }
 
         // If there is a slalom AND a regular
@@ -95,23 +95,20 @@ public class Ski {
         if (rightNodeString.equals("SlalomSegment") && leftNodeString.equals("SkiSegment")) { // right child
             SlalomSegment slalomSegRight = (SlalomSegment) rightNode.getData();
 
-            if (slalomSegRight.getDirection() == "W") return node.getLeft(); // test for windward 
+            if (slalomSegRight.getDirection().equals("W")) return leftNode; // test for windward 
             return rightNode;
         }
 
         if (leftNodeString.equals("SlalomSegment") && rightNodeString.equals("SkiSegment")) { // left child
             SlalomSegment slalomSegLeft = (SlalomSegment) leftNode.getData();
 
-            if (slalomSegLeft.getDirection() == "W") return rightNode; // test for windward
+            if (slalomSegLeft.getDirection().equals("W")) return rightNode; // test for windward
             return leftNode;
         }
 
         if (leftNodeString.equals("SkiSegment") && rightNodeString.equals("SkiSegment")) return rightNode;
-
-        return null;
-
+        return rightNode;
     }
-// update
 
 }
 
--- Assign4/TreeBuilder.java
+++ Assign4/TreeBuilder.java
@@ -3,43 +3,42 @@ public class TreeBuilder<T> {
     public LinkedBinaryTree<T> buildTree(T[] data) {
         LinkedQueue<T> dataQueue = new LinkedQueue<>();
 
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) { // go through the data and enqueue it
             dataQueue.enqueue(data[i]);
         }
 
         LinkedQueue<BinaryTreeNode<T>> parentQueue = new LinkedQueue<>();
 
-        LinkedBinaryTree<T> tree = new LinkedBinaryTree<>(dataQueue.first());
+        LinkedBinaryTree<T> dataTree = new LinkedBinaryTree<>(dataQueue.dequeue());
         
-        parentQueue.enqueue(tree.getRoot());
+        parentQueue.enqueue(dataTree.getRoot());
 
         while (!dataQueue.isEmpty()) {
 
             // if getting children nodes you need to continue to dequeue until get a parent
-            T a = dataQueue.dequeue();
-            
+            BinaryTreeNode<T> a = new BinaryTreeNode<T>(dataQueue.dequeue());
+            BinaryTreeNode<T> b = new BinaryTreeNode<T>(dataQueue.dequeue());
             BinaryTreeNode<T> parent = parentQueue.dequeue();
 
-            if (a != null) {
-                BinaryTreeNode<T> newA = new BinaryTreeNode<T>(a);
-                parent.setLeft(newA);
-                parentQueue.enqueue(parent.getLeft());
+            while (parent.getData() == null) { // check that the parent is not null
+                parent = parentQueue.dequeue(); // if it is, find a new parent
             }
 
-            if (dataQueue.isEmpty()) break;
+            if (a.getData() != null) { // check data of a for null
+                parent.setLeft(a); // set the left parent as the a node
+                parentQueue.enqueue(a); // enqueue it
+            }
 
-            T b = dataQueue.dequeue();
-            if (b != null) {
-                BinaryTreeNode<T> newB = new BinaryTreeNode<T>(b);
-                parent.setRight(newB);
-                parentQueue.enqueue(parent.getRight());
+
+            if (b.getData() != null) { // same as prior if statement
+                parent.setRight(b);
+                parentQueue.enqueue(b);
             }
 
         }
 
-        return tree;
+        return dataTree; // return the tree
 
     }
 
- // update
 }
